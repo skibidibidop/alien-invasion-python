bca09d2521736e7dad3e8817497c3f3b43a97729
skibidibidop <118040870+skibidibidop@users.noreply.github.com>
2023-04-13 06:39:11 +0800

    Refactor run_game()

--- ship.py
+++ ship.py
@@ -9,7 +9,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load ship image and get its rect
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load("images/ship.png")
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen
